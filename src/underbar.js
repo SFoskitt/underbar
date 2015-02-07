8a99d3e46fdb1bfdad9c9a8ad1fca7fbcc16bb54
SFoskitt <stephanie.foskitt@gmail.com>
2015-02-07 12:04:51 -0600

    _.first function completed

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
   /**
@@ -34,9 +35,10 @@
     return n === undefined ? array[0] : array.slice(0, n);
   };
 
-  // Like first, but for the last elements. If n is undefined, return just the
+  // Like first, but for the last n elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+  
   };
 
   // Call iterator(value, key, collection) for each element of collection.
